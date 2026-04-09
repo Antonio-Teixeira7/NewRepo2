4ed5fdcdb90b38adbf9a2921f38ff0a78738dba0
Antonio_Dev <antonioteixeiraj@gamsil.com>
2026-04-09 11:43:40 +0100

    Update the program for put the swaguer avaible.

--- UserSite/Program.cs
+++ UserSite/Program.cs
@@ -6,9 +6,7 @@ using UserSite.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,15 +19,10 @@ builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.MapOpenApi();
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+// Swagger sempre ligado
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
